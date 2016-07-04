4693e0a49c40941313dbd5422deaa49cf905caa5
Julia Kreutzer <JuliaKreutzer1@aol.com>
2016-07-04 18:30:23 +0100

    added optional lstm-rnn encoder

--- tf/ef.py
+++ tf/ef.py
@@ -11,19 +11,19 @@ Tensorflow implementation of the neural easy-first model
 """
 
 # Flags
-tf.app.flags.DEFINE_float("learning_rate", 0.5, "Learning rate.")
+tf.app.flags.DEFINE_float("learning_rate", 0.1, "Learning rate.")
 tf.app.flags.DEFINE_string("optimizer", "sgd", "Optimizer [sgd, adam, adagrad, adadelta, momentum, "
                                                "rmsprop]")
-tf.app.flags.DEFINE_integer("batch_size", 2,
+tf.app.flags.DEFINE_integer("batch_size", 1,
                             "Batch size to use during training.")
 tf.app.flags.DEFINE_integer("vocab_size", 10, "Vocabulary size.")
 tf.app.flags.DEFINE_string("data_dir", "/tmp", "Data directory")
 tf.app.flags.DEFINE_integer("max_train_data_size", 0,
                             "Limit on the size of training data (0: no limit).")
-tf.app.flags.DEFINE_float("max_gradient_norm", 1, "maximum gradient norm for clipping")
+tf.app.flags.DEFINE_float("max_gradient_norm", -1, "maximum gradient norm for clipping")
 tf.app.flags.DEFINE_integer("L", 7, "length of sequences")
 tf.app.flags.DEFINE_integer("K", 2, "number of labels")
-tf.app.flags.DEFINE_integer("D", 50, "dimensionality of embeddings")
+tf.app.flags.DEFINE_integer("D", 11, "dimensionality of embeddings")
 tf.app.flags.DEFINE_integer("N", 5, "number of sketches")
 tf.app.flags.DEFINE_integer("J", 100, "dimensionality of hidden layer")
 tf.app.flags.DEFINE_integer("r", 2, "context size")
@@ -31,11 +31,14 @@ tf.app.flags.DEFINE_boolean("train", True, "training model")
 tf.app.flags.DEFINE_integer("epochs", 100, "training epochs")
 tf.app.flags.DEFINE_boolean("shuffle", True, "shuffling training data before each epoch")
 tf.app.flags.DEFINE_integer("checkpoint_freq", 10, "save model every x epochs")
+tf.app.flags.DEFINE_boolean("lstm_units", 0, "number of LSTM-RNN encoder units")
 FLAGS = tf.app.flags.FLAGS
 
-def ef_embedding(inputs, labels, sketches, acc_sketches, indices, vocab_size, K, D, N, J, L, r):
+
+def ef_single_state(inputs, labels, sketches, acc_sketches, indices, vocab_size, K, D, N, J, L, r,
+                    lstm_units):
     """
-    Single-state easy-first model with embeddings
+    Single-state easy-first model with embeddings and optional LSTM-RNN encoder
     :param inputs:
     :param labels:
     :param sketches:
@@ -48,6 +51,7 @@ def ef_embedding(inputs, labels, sketches, acc_sketches, indices, vocab_size, K,
     :param J:
     :param L:
     :param r:
+    :param lstm_units:
     :return:
     """
 
@@ -68,45 +72,51 @@ def ef_embedding(inputs, labels, sketches, acc_sketches, indices, vocab_size, K,
                 scope.reuse_variables()
             with tf.name_scope("embedding"):
                 M = tf.get_variable(name="M", shape=[vocab_size, D],
-                                    initializer=tf.random_uniform_initializer(dtype=tf.float32))
+                                    initializer=tf.random_uniform_initializer(dtype=tf.float32,
+                                    minval=-1, maxval=1.0))
+                emb = tf.nn.embedding_lookup(M, x, name="H")  # LxD
+
+            if lstm_units > 0:
+                with tf.name_scope("lstm"):
+                    cell = tf.nn.rnn_cell.BasicLSTMCell(num_units=lstm_units, state_is_tuple=True)
+                    rnn_outputs, rnn_states = tf.nn.rnn(cell, [emb],
+                                    sequence_length=tf.fill([tf.shape(x)[0]], L), dtype=tf.float32)
+                    H = rnn_states.c
+                    state_size = lstm_units
+            else:
+                H = emb
+                state_size = D
 
             with tf.name_scope("alpha"):
                 w_z = tf.get_variable(name="w_z", shape=[J, 1],
                                       initializer=tf.random_uniform_initializer(dtype=tf.float32))
                 V = tf.get_variable(name="V", shape=[L, J],
                                     initializer=tf.random_uniform_initializer(dtype=tf.float32))
-                W_hz = tf.get_variable(name="W_hz", shape=[J, D],
+                W_hz = tf.get_variable(name="W_hz", shape=[J, state_size],
                                             initializer=tf.random_uniform_initializer(
                                                 dtype=tf.float32))
-                W_sz = tf.get_variable(name="W_sz", shape=[J, D*(2*r+1)],
+                W_sz = tf.get_variable(name="W_sz", shape=[J, state_size*(2*r+1)],
                                        initializer=tf.random_uniform_initializer(dtype=tf.float32))
                 W_bz = tf.get_variable(name="W_bz", shape=[J, 2*r+1],
                                        initializer=tf.random_uniform_initializer(dtype=tf.float32))
 
             with tf.name_scope("beta"):
-                W_hs = tf.get_variable(name="W_hs", shape=[D, D],
+                W_hs = tf.get_variable(name="W_hs", shape=[state_size, state_size],
                                        initializer=tf.random_uniform_initializer(dtype=tf.float32))
-                W_ss = tf.get_variable(name="W_ss", shape=[D*(2*r+1), D],
+                W_ss = tf.get_variable(name="W_ss", shape=[state_size*(2*r+1), state_size],
                                        initializer=tf.random_uniform_initializer(dtype=tf.float32))
-                w_s = tf.get_variable(name="w_s", shape=[D],
+                w_s = tf.get_variable(name="w_s", shape=[state_size],
                                       initializer=tf.random_uniform_initializer(dtype=tf.float32))
 
             with tf.name_scope("prediction"):
                 w_p = tf.get_variable(name="w_p", shape=[K],
                                        initializer=tf.random_uniform_initializer(dtype=tf.float32))
-                W_sp = tf.get_variable(name="W_sp", shape=[D, K],
+                W_sp = tf.get_variable(name="W_sp", shape=[state_size, K],
                                        initializer=tf.random_uniform_initializer(dtype=tf.float32))
 
             with tf.name_scope("paddings"):
-               # padding_s_col = tf.get_variable(shape=[[0, 0], [r, r]], name="padding_s_col", trainable=False)
-               # padding_b = tf.get_variable(shape=[[r, r]], name="padding_b", trainable=False)
-                #padding_s_col = tf.get_variable(name="padding_s_col", shape=[[0,0], [r,r]],
-                #                       initializer=tf.random_uniform_initializer(dtype=tf.float32), trainable=False)
-                #padding_b = tf.get_variable(name="padding_b", shape=[[r,r]],
-                 #                      initializer=tf.random_uniform_initializer(dtype=tf.float32), trainable=False)
                 padding_s_col = tf.constant([[0, 0], [r, r]], name="padding_s_col")
                 padding_b = tf.constant([[r, r]], name="padding_b")
-
 
             with tf.name_scope("looping"):
                 def fn(i):
@@ -120,16 +130,16 @@ def ef_embedding(inputs, labels, sketches, acc_sketches, indices, vocab_size, K,
             :param i:
             :return:
             """
-            h_i = tf.slice(H, [i, 0], [1, D], name="h_i")
+            h_i = tf.slice(H, [i, 0], [1, state_size], name="h_i")
             v_i = tf.slice(V, [i, 0], [1, J], name="v_i")
 
             S_padded = tf.pad(S, padding_s_col, "CONSTANT", name="S_padded")
-            S_sliced = tf.slice(S_padded, [0, i], [D, 2*r+1])  # slice columns around i
-            s_context = tf.reshape(S_sliced, [-1,1], name="s_context")
+            S_sliced = tf.slice(S_padded, [0, i], [state_size, 2*r+1])  # slice columns around i
+            s_context = tf.reshape(S_sliced, [-1, 1], name="s_context")
 
             b_padded = tf.pad(b, padding_b, "CONSTANT", name="padded")
             b_sliced = tf.slice(b_padded, [i-r+r], [2*r+1], name="b_sliced")
-            b_context = tf.reshape(b_sliced, [-1,1], name="b_context")
+            b_context = tf.reshape(b_sliced, [-1, 1], name="b_context")
 
             _1 = tf.matmul(W_sz, s_context)
             _2 = tf.matmul(W_bz, b_context)
@@ -199,7 +209,6 @@ def ef_embedding(inputs, labels, sketches, acc_sketches, indices, vocab_size, K,
             return n+1, b_n, S_n
 
         with tf.variable_scope("sketching"):
-            H = tf.nn.embedding_lookup(M, x, name="H")  # LxD
             n = tf.constant(1, dtype=tf.int32, name="n")
             (final_n, final_b, final_S) = tf.while_loop(
                 cond=lambda n, _1, _2: n <= N,
@@ -211,7 +220,7 @@ def ef_embedding(inputs, labels, sketches, acc_sketches, indices, vocab_size, K,
                 """
                 Score the word at index i
                 """
-                S_i = tf.slice(final_S, [0, i], [D, 1])  # state vector for this word (column)
+                S_i = tf.slice(final_S, [0, i], [state_size, 1])  # state vector for this word (column)
                 l = tf.matmul(S_i, W_sp, transpose_a=True) + w_p
                 shaped = tf.reshape(l, [K])
                 return shaped
@@ -231,23 +240,29 @@ def ef_embedding(inputs, labels, sketches, acc_sketches, indices, vocab_size, K,
 
         with tf.variable_scope("xent"):
             cross_entropy = tf.reduce_mean(-tf.reduce_sum(tf.one_hot(y, depth=K) * tf.log(p),
-                                               reduction_indices=[1]))  # avg xent over words
-        return cross_entropy, labels_pred, logits_packed
+                                           reduction_indices=[1]))  # avg xent over words
+        return cross_entropy, labels_pred, logits_packed, final_S, H
 
     outputs = []
     losses = []
     logits = []
+    final_sketches = []
+    embeddings = []
     init = True
     for x, y, S, b, word_indices in zip(inputs, labels, sketches, acc_sketches, indices):
-        loss, output, logit = forward(x, y, S, b, word_indices, init=init)
+        loss, output, logit, final_sketch, embedding = forward(x, y, S, b, word_indices, init=init)
         outputs.append(output)
         losses.append(loss)
         logits.append(logit)
+        final_sketches.append(final_sketch)
+        embeddings.append(embedding)
         init = False
     outputs = tf.pack(outputs)
     losses = tf.pack(losses)
     logits = tf.pack(logits)
-    return outputs, losses, logits
+    final_sketches = tf.pack(final_sketches)
+    embeddings = tf.pack(embeddings)
+    return outputs, losses, logits, final_sketches, embeddings
 
 
 class EasyFirstModel():
@@ -255,7 +270,7 @@ class EasyFirstModel():
     Neural easy-first model
     """
     def __init__(self, K, D, N, J, L, r, vocab_size, batch_size, optimizer, learning_rate,
-                 max_gradient_norm, forward_only=False):
+                 max_gradient_norm, lstm_units, forward_only=False):
         """
         Initialize the model
         :param K:
@@ -264,6 +279,7 @@ class EasyFirstModel():
         :param J:
         :param L:
         :param r:
+        :param lstm_units:
         :param vocab_size:
         :param batch_size:
         :param optimizer:
@@ -278,6 +294,7 @@ class EasyFirstModel():
         self.J = J
         self.L = L
         self.r = r
+        self.lstm_units = lstm_units
         self.vocab_size = vocab_size
         self.batch_size = batch_size
         self.learning_rate = learning_rate
@@ -294,10 +311,18 @@ class EasyFirstModel():
         self.sketches = []
         self.acc_sketches = []
         self.indices = []
+        if self.lstm_units > 0:
+            self.state_size = self.lstm_units
+            print "Model with LSTM RNN encoder of %d units and embeddings of size %d" % \
+                  (self.lstm_units, self.D)
+        else:
+            self.state_size = D
+            print "Model with simple embeddings of size %d" % self.D
+
         for i in xrange(batch_size):  # feed batch_size many xs and ys to graph
             self.inputs.append(tf.placeholder(tf.int32, shape=[self.L], name="input{0}".format(i)))
             self.labels.append(tf.placeholder(tf.int32, shape=[self.L], name="label{0}".format(i)))
-            self.sketches.append(tf.placeholder(tf.float32, shape=[self.D, self.L],
+            self.sketches.append(tf.placeholder(tf.float32, shape=[self.state_size, self.L],
                                                 name="sketch{0}".format(i)))
             self.acc_sketches.append(tf.placeholder(tf.float32, shape=[self.L],
                                                     name="acc_sketch{0}".format(i)))
@@ -305,12 +330,13 @@ class EasyFirstModel():
                                                name="indices{0}".format(i)))
 
         def ef_f(inputs, labels, sketches, acc_sketches, indices):
-            return ef_embedding(inputs, labels, sketches, acc_sketches, indices,
-                                vocab_size=vocab_size, K=self.K, D=self.D, N=self.N,
-                                J=self.J, L=self.L, r=self.r)
 
-        self.outputs, self.losses, self.logits = ef_f(self.inputs, self.labels, self.sketches, self.acc_sketches,
-                                         self.indices)
+            return ef_single_state(inputs, labels, sketches, acc_sketches, indices,
+                                    vocab_size=vocab_size, K=self.K, D=self.D, N=self.N,
+                                    J=self.J, L=self.L, r=self.r, lstm_units=lstm_units)
+
+        self.outputs, self.losses, self.logits, self.final_sketches, self.embeddings = \
+            ef_f(self.inputs, self.labels, self.sketches, self.acc_sketches, self.indices)
 
         # gradients and update operation for training the model
         if not forward_only:
@@ -345,7 +371,7 @@ class EasyFirstModel():
         for l in xrange(self.batch_size):
             # variables dependent on sequence length
             L_i = len(inputs[l])
-            S_0 = np.zeros((FLAGS.D, L_i), dtype=float)
+            S_0 = np.zeros((self.state_size, L_i), dtype=float)
             b_0 = np.array([1./L_i]*L_i)
             #print S_0, b_0
             word_indices_i = np.arange(L_i)
@@ -356,21 +382,19 @@ class EasyFirstModel():
             input_feed[self.indices[l].name] = word_indices_i
 
         if not forward_only:
-            output_feed = [self.updates,  # Update Op that does SGD.
-                     self.gradient_norms,  # Gradient norm.
+            output_feed = [self.updates,
+                     self.gradient_norms,
                      self.losses,
                      self.outputs,
-                     self.logits]  # Loss for this batch.
+                     self.logits,
+                     self.final_sketches,
+                     self.embeddings]
         else:
-            output_feed = [self.losses, self.outputs]  # loss and predictions for this batch
-            #for l in xrange(inputs):  # Output logits.
-            #    output_feed.append(self.outputs[l])
-        #print "input_feed", input_feed.keys()
-        #print "output_feed", output_feed
+            output_feed = [self.losses, self.outputs]
 
         outputs = session.run(output_feed, input_feed)
         if not forward_only:
-            return outputs[1], outputs[2], outputs[3], outputs[4] # Gradient norm, loss, predictions
+            return outputs[1], outputs[2], outputs[3], outputs[4], outputs[5], outputs[6]
         else:
             return outputs[0], outputs[1]  # loss, predictions
 
@@ -384,7 +408,7 @@ def create_model(session, forward_only=False):
     """
     model = EasyFirstModel(FLAGS.K, FLAGS.D, FLAGS.N, FLAGS.J, FLAGS.L, FLAGS.r, FLAGS.vocab_size,
                            FLAGS.batch_size, FLAGS.optimizer, FLAGS.learning_rate,
-                           FLAGS.max_gradient_norm, forward_only)
+                           FLAGS.max_gradient_norm, FLAGS.lstm_units, forward_only)
     # TODO add loading from checkpoint
     session.run(tf.initialize_all_variables())
     return model
@@ -403,12 +427,12 @@ def train():
 
         # TODO read the data
         # dummy data
-        no_train_instances = 10
+        no_train_instances = 40
         X_train = np.maximum(np.round(
             np.random.rand(no_train_instances, FLAGS.L)*FLAGS.vocab_size-1, 0), 0)
         Y_train = np.round(np.random.rand(no_train_instances, FLAGS.L), 0)
 
-        no_dev_instances = 4
+        no_dev_instances = 10
         X_dev = np.maximum(np.round(
             np.random.rand(no_dev_instances, FLAGS.L)*FLAGS.vocab_size-1, 0), 0)
         Y_dev = np.round(np.random.rand(no_dev_instances, FLAGS.L), 0)
@@ -424,17 +448,17 @@ def train():
 
             while current_sample < len(X_train):
 
-                x_i = X_train[current_sample:current_sample+FLAGS.batch_size]
+                x_i = X_train[current_sample:current_sample+FLAGS.batch_size]  # TODO what if train_size % batch_size !=0
                 y_i = Y_train[current_sample:current_sample+FLAGS.batch_size]
 
                 start_time = time.time()
-                _, step_loss, predictions, logits = model.step(sess, x_i, y_i, False)
+                _, step_loss, predictions, logits, final_sketches, embeddings = model.step(sess, x_i, y_i, False)
 
                 step_time += (time.time() - start_time)
                 loss += np.sum(step_loss)
-                print current_sample, x_i, _, y_i, predictions, step_loss, logits
+                #print current_sample, x_i, _, y_i, predictions, step_loss, embeddings
+                # TODO regularizer
                 train_predictions.append(predictions)
-                #print current_sample, predictions
 
                 current_sample += FLAGS.batch_size
 
@@ -482,6 +506,8 @@ def test():
     """
     print "Testing"
     # TODO
+    # load model
+    # eval
 
 
 def main(_):
