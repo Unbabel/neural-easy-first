01ab897c04b90565e5ef87f67655e4887416f6ee
juliakreutzer <juliakreutzer1@aol.com>
2016-09-29 18:01:35 +0100

    improved sketch drawing

--- tf/draw_sketches.py
+++ tf/draw_sketches.py
@@ -1,33 +1,49 @@
 import cPickle as pkl
-import numpy as np
 import matplotlib.pyplot as plt
+import argparse
 
-if __name__ == "__main__":
 
-    epochs = 12
-    sent_id = 37 #793
-    N = 9 #20
-
-    cols = N
-    rows = epochs
+def main(args):
+    """
+    Plot the attention (and sketches) for a single example across epochs and sketches
+    :param args:
+    :return:
+    """
+    cols = args.sent_len
+    rows = args.epochs
     fig, axes = plt.subplots(nrows=rows, ncols=cols)
-    for epoch in xrange(epochs):
+    for epoch in xrange(args.epochs):
 
-        sketch_file = "sketches/sketches_sent%d_epoch%d.pkl" % (sent_id, epoch)  # sketches for one sample sentence for one epoch
-        #sketch_file = "sketches/hmm-nef/sketches_sent%d_epoch%d.pkl" % (sent_id, epoch)  # sketches for one sample sentence for one epoch
-        print sketch_file
+        sketch_file = args.sketch_dir+'/sketches_sent%d_epoch%d.pkl' % (args.sent_id, epoch)  # sketches for one sample sentence for one epoch
         # load sketches from pkl object
-        all_sketches = pkl.load(open(sketch_file, "rb"))  # shape: (N, sequence len, state_size)
-
+        all_sketches = pkl.load(open(sketch_file, "rb"))  # shape: (N, sequence len, sketch_size+1)
         # color sketches for samples over time
         for i, sketch in enumerate(all_sketches):
-            print sketch
             ax = axes[epoch][i] #[epoch*cols+i+1]
-            im = ax.imshow(sketch.transpose()[-2:-1], interpolation="none", vmin=0.0, vmax=0.5)
-            #ax.set_title("Epoch %d, sketch %d" % (epoch+1, i+1))
+            print args.plot_sketches
+            if args.plot_sketches:
+                plot_values = sketch
+            else:
+                plot_values = sketch[:,-1:]  # just plot attention
+            im = ax.imshow(plot_values.transpose(), interpolation="none", vmin=0.0, vmax=0.5)
+            ax.get_yaxis().set_visible(False)
+            if epoch < args.epochs-1:
+                ax.get_xaxis().set_visible(False)
     fig.subplots_adjust(right=0.8)
     cbar_ax = fig.add_axes([0.85, 0.15, 0.05, 0.7])
     fig.colorbar(im, cax=cbar_ax)
     plt.show()
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Plot sketches for example sentence')
+    parser.add_argument('epochs', type=int, help='number of epochs to plot')
+    parser.add_argument('sent_id', type=int, help='sentence to plot')
+    parser.add_argument('sent_len', type=int, help='length of sentence')
+    parser.add_argument('sketch_dir', type=str, help='directory where sketches are dumped')
+    parser.add_argument('--plot_sketches', action='store_true', help='plot sketches in addition to the attention')
+    args = parser.parse_args()
+    main(args)
+
+
 
 
