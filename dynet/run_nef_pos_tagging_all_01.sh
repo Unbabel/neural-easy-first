94c60599363b2c0710a16b086938700971fc0a66
Andre Martins <andre.t.martins@gmail.com>
2017-01-20 22:44:32 +0000

    Slight changes in scripts.

--- dynet/run_nef_pos_tagging_all_01.sh
+++ dynet/run_nef_pos_tagging_all_01.sh
@@ -1,9 +1,9 @@
 language=$1
 attention_type=$2
-model_type=single_state
+model_type=all_states
 temperature=1.
 discount_factor=0.
-num_sketches=-1
+num_sketches=$3 #-1
 use_sketch_losses=0
 embedding_size=64
 hidden_size=50
@@ -12,36 +12,7 @@ context_size=2
 noise_level=0.
 affix_length=4
 affix_embedding_size=50
-
-concatenate_last_layer=0
-use_max_pooling=0
-sum_hidden_states_and_sketches=0
-share_attention_sketch_parameters=0
-sketch_size=50
-for l2_regularization in 0.001 0
-do
-    ./run_nef_pos_tagging.sh \
-        ${model_type} \
-        ${attention_type} \
-        ${temperature} \
-        ${discount_factor} \
-        ${l2_regularization} \
-        ${num_sketches} \
-        ${concatenate_last_layer} \
-        ${use_sketch_losses} \
-        ${use_max_pooling} \
-        ${sum_hidden_states_and_sketches} \
-        ${share_attention_sketch_parameters} \
-        ${embedding_size} \
-        ${hidden_size} \
-        ${preattention_size} \
-        ${sketch_size} \
-        ${context_size} \
-        ${noise_level} \
-        ${affix_length} \
-        ${affix_embedding_size} \
-        ${language}
-done
+use_bilstm=1
 
 concatenate_last_layer=1
 use_max_pooling=0
@@ -70,6 +41,38 @@ do
         ${noise_level} \
         ${affix_length} \
         ${affix_embedding_size} \
+        ${use_bilstm} \
+        ${language}
+done
+
+concatenate_last_layer=0
+use_max_pooling=0
+sum_hidden_states_and_sketches=1
+share_attention_sketch_parameters=0
+sketch_size=100
+for l2_regularization in 0.001 0
+do
+    ./run_nef_pos_tagging.sh \
+        ${model_type} \
+        ${attention_type} \
+        ${temperature} \
+        ${discount_factor} \
+        ${l2_regularization} \
+        ${num_sketches} \
+        ${concatenate_last_layer} \
+        ${use_sketch_losses} \
+        ${use_max_pooling} \
+        ${sum_hidden_states_and_sketches} \
+        ${share_attention_sketch_parameters} \
+        ${embedding_size} \
+        ${hidden_size} \
+        ${preattention_size} \
+        ${sketch_size} \
+        ${context_size} \
+        ${noise_level} \
+        ${affix_length} \
+        ${affix_embedding_size} \
+        ${use_bilstm} \
         ${language}
 done
 
@@ -102,6 +105,7 @@ do
         ${noise_level} \
         ${affix_length} \
         ${affix_embedding_size} \
+        ${use_bilstm} \
         ${language}
 done
 
@@ -132,6 +136,7 @@ do
         ${noise_level} \
         ${affix_length} \
         ${affix_embedding_size} \
+        ${use_bilstm} \
         ${language}
 done
 fi
